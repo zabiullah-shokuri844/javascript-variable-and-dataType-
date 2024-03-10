4220a0a1c89bf63bbfc74fca6c43b34612304e1f
zabiullah-shokuri4 <zabitawana4@gmail.com>
2024-03-10 15:17:06 +0330

    javascript variable

---
+++ variable.js
@@ -0,0 +1,121 @@
+
+
+// let , var , const 
+
+
+// let count = 0;
+count = 1; 
+console.log(count);
+
+// const PI = 3.14;
+PI = 4;
+console.log(PI);
+
+
+var count = 0;
+if (true) {
+    var count = 1;
+    console.log(count)
+}
+console.log(count);
+
+
+// Data Types in JavaScript 
+
+// Exp: 
+
+// let x = 5;
+Number 
+console.log(typeof x);
+
+BigInt 
+const BigInt = 123456789123456789n;
+console.log(typeof BigInt);
+
+
+string 
+let  name = 'Eng_Zabi';
+console.log(name)
+
+// Boolean 
+
+let isTrue = false;
+console.log(isTrue);
+
+// null 
+
+// let x = null;
+// console.log(x);
+
+// undefined 
+
+var age;
+console.log(age)
+
+
+// Object 
+
+let person = {
+    name: 'Eng_zabi',
+    age: 26
+}
+
+
+// Symbol 
+
+let id = Symbol('id');
+console.log(Symbol);
+
+// // Using arithmetic and comparison operators
+
+// // arithmetic Operators
+
+// // Exp:
+let x = 10;
+let y = 5;
+let sum = x + y  //Addition 
+console.log(sum);
+
+// // Subraction (-) 
+let difference = x - y;
+console.log(difference);
+
+
+// Division 
+let quotient = x / y;
+console.log(quotient);
+
+// // Multiplication 
+let product = x * y;
+console.log(product);
+
+// // Modulus (%)
+
+let remainder = x % y;
+console.log(remainder)
+
+// Exponentiation (**)
+let result = x ** y;
+console.log(result);
+
+
+// Using arithmetic and Comparison operators
+
+// Exp: 
+
+let a = 10;
+let b = 5;
+console.log(a == b);  //false
+console.log(a != b); // true
+
+console.log(a === b);  //false
+console.log(a !== b);  //True
+
+console.log(a > b);  //True
+
+console.log(a >= b)  //True
+
+console.log(a < b);  //False
+console.log(a <= b); //False
+
+
